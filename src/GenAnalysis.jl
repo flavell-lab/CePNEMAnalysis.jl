163a0955c25b1262ce94d59f766037098dfb89a3
goldenratio1618 <adamatanas@gmail.com>
2022-03-28 16:46:26 -0400

    Add dependencies

--- src/GenAnalysis.jl
+++ src/GenAnalysis.jl
@@ -2,6 +2,14 @@ module GenAnalysis
 
 using FlavellBase
 using EncoderModelGen
+using HDF5
+using Gen
+using Statistics
+using StatsBase
+using ProgressMeter
+using Plots
+using TSne
+using ColorSchemes
 
 include("data.jl")
 

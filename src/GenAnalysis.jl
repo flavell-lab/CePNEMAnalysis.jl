83b3eecd206f4ee442262255c78bce67c8b586b5
goldenratio1618 <adamatanas@gmail.com>
2022-03-28 17:55:48 -0400

    Added neuron categorization and p-value computation methods.

--- src/GenAnalysis.jl
+++ src/GenAnalysis.jl
@@ -12,9 +12,26 @@ using TSne
 using ColorSchemes
 
 include("data.jl")
+include("dist.jl")
+include("categorize.jl")
+include("plot.jl")
 
 export
     # data.jl
-    load_gen_output
-
+    load_gen_output,
+    # dist.jl
+    update_cmap!,
+    evaluate_pdf_xgiveny!,
+    compute_dists,
+    kl_div,
+    overlap_index,
+    prob_P_greater_Q,
+    # categorize.jl
+    deconvolved_model_nl7b,
+    compute_range,
+    get_deconvolved_activity,
+    neuron_p_vals,
+    categorize_neurons,
+    # plot.jl
+    make_deconv_heatmap
 end # module

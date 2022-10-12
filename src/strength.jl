91b3623a37f79e613ff5102f5c80dcd86645ed4c
Adam Atanas <adamatanas@gmail.com>
2022-10-12 16:41:55 -0400

    Updated y0 to mean of deconvolved model, for maximum comparability

--- src/strength.jl
+++ src/strength.jl
@@ -27,7 +27,7 @@ function get_relative_encoding_strength_mt(fit_results, dataset, rng, neuron; ma
     std_deconv_P = zeros(max_idx)
 
     std_full = zeros(max_idx)
-    std_full_nops6 = zeros(max_idx)
+    std_full_ps6corr = zeros(max_idx)
     std_v = zeros(max_idx)
     std_θh = zeros(max_idx)
     std_P = zeros(max_idx)
@@ -50,20 +50,28 @@ function get_relative_encoding_strength_mt(fit_results, dataset, rng, neuron; ma
         model_noθh = model_nl8(max_t, ps..., b_v, b_null, b_P)
         model_noP = model_nl8(max_t, ps..., b_v, b_θh, b_null)
 
-        ps[6] = 0 # delete initial condition so as not to contaminate deconvolution computation
-
-        model_full_nops6 = model_nl8(max_t, ps..., b_v, b_θh, b_P)
-        model_v = model_nl8(max_t, ps..., b_v, b_null, b_null)
-        model_θh = model_nl8(max_t, ps..., b_null, b_θh, b_null)
-        model_P = model_nl8(max_t, ps..., b_null, b_null, b_P)
-
+        mean_v = mean(b_v)
+        mean_θh = mean(b_θh)
+        mean_P = mean(b_P)
         
         for i_t = 1:length(rng_t)
             model_deconv[idx, i_t] = deconvolved_model_nl8(ps, b_v[i_t], b_θh[i_t], b_P[i_t])
             model_deconv_v[idx, i_t] = deconvolved_model_nl8(ps, b_v[i_t], b_null[i_t], b_null[i_t])
             model_deconv_θh[idx, i_t] = deconvolved_model_nl8(ps, b_null[i_t], b_θh[i_t], b_null[i_t])
             model_deconv_P[idx, i_t] = deconvolved_model_nl8(ps, b_null[i_t], b_null[i_t], b_P[i_t])
-        end        
+        end       
+        
+        ps[6] = mean(model_deconv) # set initial condition to mean so as not to contaminate convolution computation
+        model_full_ps6corr = model_nl8(max_t, ps..., b_v, b_θh, b_P)
+
+        ps[6] = mean(model_deconv_v)
+        model_v = model_nl8(max_t, ps..., b_v, b_null, b_null)
+
+        ps[6] = mean(model_deconv_θh)
+        model_θh = model_nl8(max_t, ps..., b_null, b_θh, b_null)
+
+        ps[6] = mean(model_deconv_P)
+        model_P = model_nl8(max_t, ps..., b_null, b_null, b_P)
         
         std_full[idx] = std(model_full)
         std_v[idx] = std(model_v)
@@ -86,5 +94,5 @@ function get_relative_encoding_strength_mt(fit_results, dataset, rng, neuron; ma
         err_P[idx] = eP / s
     end
     
-    return err_v, err_θh, err_P, std_full, std_full_nops6, std_v, std_θh, std_P, std_deconv, std_deconv_v, std_deconv_θh, std_deconv_P
+    return err_v, err_θh, err_P, std_full, std_full_ps6corr, std_v, std_θh, std_P, std_deconv, std_deconv_v, std_deconv_θh, std_deconv_P
 end
